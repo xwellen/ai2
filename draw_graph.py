dc6bfffeda66efbb074b328515d759aa77ce532d
Sergey Ubozhenko <xwellen@gmail.com>
2022-12-06 21:26:05 +0300

    final commit

--- draw_graph.py
+++ draw_graph.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 
+
 def draw_graph(G, pos, title_string="Untitled"):
     nx.draw_networkx_edges(G, pos, edgelist=G.edges)
     nx.draw_networkx_nodes(G, pos, node_size=20)
--- find_path_and_draw.py
+++ find_path_and_draw.py
@@ -4,13 +4,15 @@ import matplotlib.pyplot as plt
 import networkx as nx
 
 
-def find_path_and_draw(graph, subgraph_edges, pos, title_string="Untitled", print_nodes=True, mark_destination=True, mark_red=True):
+def find_path_and_draw(graph, subgraph_edges, pos, title_string="Untitled", print_nodes=True, mark_destination=True,
+                       mark_red=True, source_node=os.getenv("SOURCE_NODE"),
+                       destination_node=os.getenv("DESTINATION_NODE")):
     G__ = nx.Graph()
     G__.add_edges_from(subgraph_edges)
-    current_shortest_path_verts = nx.shortest_path(G__, os.getenv("SOURCE_NODE"), os.getenv("DESTINATION_NODE"))
+    current_shortest_path_nodes = nx.shortest_path(G__, source_node, destination_node)
     current_path = []
-    for i in range(len(current_shortest_path_verts) - 1):
-        current_path.append((current_shortest_path_verts[i], current_shortest_path_verts[i + 1]))
+    for i in range(len(current_shortest_path_nodes) - 1):
+        current_path.append((current_shortest_path_nodes[i], current_shortest_path_nodes[i + 1]))
     if print_nodes:
         print("path:")
         print(current_path)
@@ -26,11 +28,17 @@ def find_path_and_draw(graph, subgraph_edges, pos, title_string="Untitled", prin
     nx.draw_networkx_nodes(graph, pos, node_size=20)
     # source and dest nodes
     if mark_destination:
-        nx.draw_networkx_nodes(graph, pos, nodelist=[os.getenv("SOURCE_NODE"), os.getenv("DESTINATION_NODE")],
+        nx.draw_networkx_nodes(graph, pos, nodelist=[source_node, destination_node],
                                node_size=20, node_color='#ffee00')
     else:
-        nx.draw_networkx_nodes(graph, pos, nodelist=[os.getenv("SOURCE_NODE")],
+        nx.draw_networkx_nodes(graph, pos, nodelist=[source_node],
                                node_size=20, node_color='#ffee00')
+
+    path_labels_dict = dict()
+    for node in current_shortest_path_nodes:
+        path_labels_dict.update({node: str(node)})
+
+    nx.draw_networkx_labels(G__, pos, path_labels_dict, font_size=8)
     plt.title(title_string)
     plt.axis("off")
     plt.show()
